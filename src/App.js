96d32bcef7ca2adae6038dac5dfed85345e5eb82
Enes Doğan <enesdoganlive@hotmail.com>
2023-04-29 23:37:30 +0300

    Project modified

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
+import "./App.css";
 import React, { useState } from "react";
 import Weather from "./components/Weather";
-import Popup from "./components/Popup";
-import "./App.css";
-import axios from "axios";
 
 function App() {
-  const [city, setCity] = useState("");
-  const [weatherData, setWeatherData] = useState({});
-  const [showPopup, setShowPopup] = useState(false);
-
-  const handleSearch = (city) => {
-    setCity(city);
-    setShowPopup(true);
+  const APP_KEY = process.env.REACT_APP_WEATHER_DATA;
+  let cityInput = "";
+  const [weatherData, setWeatherData] = useState([]);
+  let cityText = () => {
+    document.querySelector("input").addEventListener("input", (e) => {
+      e.preventDefault();
+      cityInput = e.target.value;
+    });
   };
 
-  const handleClosePopup = () => {
-    setShowPopup(false);
-  };
-
-  const handleWeatherData = (data) => {
-    setWeatherData(data);
-  };
+  async function getData(value) {
+    const data =
+      await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${APP_KEY}&q=${value}&days=3&aqi=no&alerts=no
+    `);
+    const result = await data.json();
+    setWeatherData(result.forecast.forecastday);
+    console.log(result.forecast.forecastday);
+  }
 
   return (
     <div className="app">
-      <div>
-        <h1 className="app-title text-dark">Weather App</h1>
-        <Weather onSearch={handleSearch} onData={handleWeatherData} />
-        {showPopup && <Popup onClose={handleClosePopup} data={weatherData} />}
+      <h1 className="app-title text-dark">Weather App</h1>
+      <div className="search">
+        <input type="text" placeholder="Search a city.." onChange={cityText} />
+        <button onClick={() => getData(cityInput)}>Search</button>
       </div>
+      {weatherData.map((item) => (
+        <Weather
+          key={item.date}
+          date={item.date}
+          minTemp={item.day.mintemp_c}
+          maxTemp={item.day.maxtemp_c}
+          condition={item.day.condition.text}
+          icon={item.day.condition.icon}
+        />
+      ))}
       <footer>Designed by Enes Doğan | &copy; 2023</footer>
     </div>
   );
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,154 +1,19 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Popup from "./Popup";
-import "../App.css";
+import React from "react";
+import "../css/weather.css";
 
-function Weather() {
-  const [city, setCity] = useState("");
-  const [weatherData, setWeatherData] = useState(null);
-  const [popupOpen, setPopupOpen] = useState(false);
-  const [weatherForecast, setWeatherForecast] = useState([]);
-  const apiKey = process.env.REACT_APP_WEATHER_DATA;
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const cityName = e.target.elements.city.value;
-    if (!cityName) {
-      return;
-    } else {
-      setCity(e.target.elements.city.value);
-      setPopupOpen(true);
-    }
-  };
-
-  const handlePopupClose = () => {
-    setPopupOpen(false);
-    setWeatherData(null);
-  };
-
-  const fetchWeatherData = async () => {
-    try {
-      const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
-      );
-      setWeatherData(response.data);
-    } catch (error) {
-      alert(error);
-    }
-  };
-
-  const fetchWeatherForecast = async () => {
-    try {
-      const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`
-      );
-      setWeatherForecast(response.data.list.slice(0, 8));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    if (city) {
-      fetchWeatherData();
-    }
-  }, [city]);
-
-  const today = weatherForecast.length
-    ? new Date(weatherForecast[0].dt * 1000).toLocaleDateString("en-US", {
-        weekday: "long",
-      })
-    : "";
-  const tomorrow = weatherForecast.length
-    ? new Date(weatherForecast[1].dt * 1000).toLocaleDateString("en-US", {
-        weekday: "long",
-      })
-    : "";
-  const dayAfterTomorrow = weatherForecast.length
-    ? new Date(weatherForecast[2].dt * 1000).toLocaleDateString("en-US", {
-        weekday: "long",
-      })
-    : "";
-  const dayAfterDayAfterTomorrow = weatherForecast.length
-    ? new Date(weatherForecast[3].dt * 1000).toLocaleDateString("en-US", {
-        weekday: "long",
-      })
-    : "";
-
+function Weather({ date, minTemp, maxTemp, condition, icon }) {
   return (
-    <div className="weather-app">
-      {popupOpen ? (
-        <Popup handleClose={handlePopupClose}>
-          {weatherForecast.length > 0 && (
-            <table>
-              <thead>
-                <tr>
-                  <th>Time</th>
-                  <th>Weather</th>
-                  <th>Temperature</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>{today}</td>
-                  <td>
-                    <img
-                      src={`http://openweathermap.org/img/w/${weatherForecast[0].weather[0].icon}.png`}
-                      alt="Weather icon"
-                    />
-                  </td>
-                  <td>{weatherForecast[0].main.temp}°C</td>
-                </tr>
-                <tr>
-                  <td>{tomorrow}</td>
-                  <td>
-                    <img
-                      src={`http://openweathermap.org/img/w/${weatherForecast[1].weather[0].icon}.png`}
-                      alt="Weather icon"
-                    />
-                  </td>
-                  <td>{weatherForecast[1].main.temp}°C</td>
-                </tr>
-                <tr>
-                  <td>{dayAfterTomorrow}</td>
-                  <td>
-                    <img
-                      src={`http://openweathermap.org/img/w/${weatherForecast[2].weather[0].icon}.png`}
-                      alt="Weather icon"
-                    />
-                  </td>
-                  <td>{weatherForecast[2].main.temp}°C</td>
-                </tr>
-                <tr>
-                  <td>{dayAfterDayAfterTomorrow}</td>
-                  <td>
-                    <img
-                      src={`http://openweathermap.org/img/w/${weatherForecast[3].weather[0].icon}.png`}
-                      alt="Weather icon"
-                    />
-                  </td>
-                  <td>{weatherForecast[3].main.temp}°C</td>
-                </tr>
-              </tbody>
-            </table>
-          )}
-        </Popup>
-      ) : (
-        <form onSubmit={handleSubmit} className="d-flex">
-          <input
-            className="d-inline form-control input-lg"
-            type="text"
-            name="city"
-            placeholder="Enter city name..."
-          />
-          <button
-            className="btn mx-1 bg-success text-light rounded"
-            type="submit"
-          >
-            Search
-          </button>
-        </form>
-      )}
+    <div className="result">
+      <h2>{date}</h2>
+      <ul>
+        <li>
+          <img src={icon} alt="" />
+        </li>
+        <li>{condition}</li>
+        <li>
+          {minTemp} °C / {maxTemp} C
+        </li>
+      </ul>
     </div>
   );
 }
