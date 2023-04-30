b00dcd4c6b64533a2edafa1845f3e9d0801bf7ff
Enes Doğan <enesdoganlive@hotmail.com>
2023-04-30 11:36:55 +0300

    Project modified

--- src/App.js
+++ src/App.js
@@ -4,36 +4,50 @@ import Weather from "./components/Weather";
 
 function App() {
   const APP_KEY = process.env.REACT_APP_WEATHER_DATA;
-  let cityInput = "";
+  const [cityInput, setCityInput] = useState("");
   const [weatherData, setWeatherData] = useState([]);
-  let cityText = () => {
-    document.querySelector("input").addEventListener("input", (e) => {
-      e.preventDefault();
-      cityInput = e.target.value;
-    });
+  const [error, setError] = useState("");
+
+  const handleInputChange = (event) => {
+    setCityInput(event.target.value);
   };
 
-  async function getData(value) {
-    const data =
-      await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${APP_KEY}&q=${value}&days=3&aqi=no&alerts=no
-    `);
-    const result = await data.json();
-    setWeatherData(result.forecast.forecastday);
-    console.log(result.forecast.forecastday);
-  }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!cityInput) {
+      setError("Please enter a city name.");
+      return;
+    }
+    try {
+      const response = await fetch(
+        `http://api.weatherapi.com/v1/forecast.json?key=${APP_KEY}&q=${cityInput}&days=3&aqi=no&alerts=no`
+      );
+      const result = await response.json();
+      if (result.error) {
+        setError(result.error.message);
+      } else {
+        setWeatherData(result.forecast.forecastday);
+        setError("");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
   return (
     <div className="app">
+      <h1 className="app-title text-dark">Weather App</h1>
       <div className="app-items">
-        <h1 className="app-title text-dark">Weather App</h1>
-        <div className="search">
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
             placeholder="Search a city.."
-            onChange={cityText}
+            value={cityInput}
+            onChange={handleInputChange}
           />
-          <button onClick={() => getData(cityInput)}>Search</button>
-        </div>
+          <button type="submit">Search</button>
+        </form>
+        {error && <div className="error">{error}</div>}
       </div>
       {weatherData.map((item) => (
         <Weather
